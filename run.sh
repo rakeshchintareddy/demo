7abbe2a26e6f5efdacedef731c1316dc6d09df44
Rakesh Reddy Chintareddy <rakeshchintareddy@gmail.com>
2018-03-20 22:50:32 -0400

    Added script to run maven

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+cd $(dirname $0)
+
+mvn clean compile
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
+
+mvn clean package
+
+rm -rf target
+
+rm -rf build
+exit
